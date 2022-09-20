ede517ee9bde767612be6be3d0ecb43fb37aab45
DESKTOP-BN9J5DT\User <ysh0214727@gmail.com>
2022-09-20 18:12:30 +0900

    Remove adder files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./App.css";
 import Logo from './Logo';
 import Hospital from './Hospital';
-import SimpleMap from './SimpleMap';
+/*import SimpleMap from './SimpleMap';*/
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from "framer-motion";
 
