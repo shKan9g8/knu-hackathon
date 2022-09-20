6696979e543e3b3dfea7b91a9ef280fee9c74d93
solorsystem1 <39077221+solorsystem1@users.noreply.github.com>
2022-09-20 19:00:09 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import "./App.css";
 import Logo from './Logo';
 import Hospital from './Hospital';
-<<<<<<< HEAD
-=======
-/*import SimpleMap from './SimpleMap';*/
->>>>>>> 8eec1fe95ef4d50c9faaccfa36a3d6f67fd24d00
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from "framer-motion";
 
