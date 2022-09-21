6acac1b2dc38c2837d6888606083c209c55596ef
SungHyun-Kang <80497157+SungHyun-Kang@users.noreply.github.com>
2022-09-21 23:52:38 +0900

    메뉴
    제목 출력, 버튼 출력 상호작용

--- src/Hospital.js
+++ src/Hospital.js
@@ -1,32 +1,26 @@
-import React from 'react';
+import React,{useRef, useState} from 'react';
 import "./Hospital.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { motion } from "framer-motion";
-import Menu from "./component/Menu.js"
-import styled from 'styled-components';
 import Map from "./component/Map.js";
 
 export default function Hospital() {
-  const Content = styled.div`
-  ${(props) => props.theme.main};
-  width: 100%;
-  height: 100%;
-`;
+
     return ( 
         <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{delay:1}} >
                 <div className='container'>
                     <div>
                         Logo & title
                     </div>
-                    <div className = 'selector-container'>
-                      MENU
-                      <Menu />
+                    <div className='selector-container'>
+                        Menu
                     </div>
                     <div className='map-container'>
-                        <Map />
+                        <Map/>
                     </div>
-                    {/* <div>
-                        {Menu.content && <Content a = {state}>{Menu.selectComponent[Menu.content]}</Content>}
-                    </div> */}
+                    <div>
+                        Text
+                    </div>
 
                 </div>
         </motion.div>
