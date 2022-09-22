d0ae0ef9e73277f8d9c3b8599cd1affa13301ce4
solorsystem1 <39077221+solorsystem1@users.noreply.github.com>
2022-09-22 23:28:37 +0900

    image crawling

--- crawling-python/main.py
+++ crawling-python/main.py
@@ -2,6 +2,7 @@ import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
+import urllib.request
 
 chromedriver_autoinstaller.install(True)
 
@@ -15,9 +16,11 @@ def startChrome():
     return browser
 
 def hackerton():
+    id_list=[]
     title_list=[]
     link_list=[]
     date_list=[]
+    count=1
     for i in range(1,6):
         url = f'https://www.dgphpi.or.kr/board/lists/3?keyword=&field=&page={i}'
         browser=startChrome()
@@ -25,13 +28,17 @@ def hackerton():
         gallery_list = browser.find_element(By.CLASS_NAME,'galley_list')
         gallerys=gallery_list.find_elements(By.TAG_NAME,'li')
         for gallery in gallerys:
+            id_list.append(count)
             title_list.append(gallery.find_element(By.CLASS_NAME,'title').text)
             date_list.append(gallery.find_element(By.CLASS_NAME,'date').text)
             link_list.append(gallery.find_element(By.TAG_NAME,'a').get_attribute('href'))
+            imgUrl=gallery.find_element(By.TAG_NAME,'img').get_attribute('src')
+            urllib.request.urlretrieve(imgUrl,str(count)+".png")
+            count=count+1
 
-    return title_list, date_list, link_list
+    return id_list, title_list, date_list, link_list
 
-title_list,date_list, link_list=hackerton()
+id_list,title_list,date_list, link_list=hackerton()
 
 df=pd.DataFrame({'title':title_list,'date':date_list,'link':link_list})
 df.to_json('crawling.json',orient='records',force_ascii=False)
