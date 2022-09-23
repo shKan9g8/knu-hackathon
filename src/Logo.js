fbcceb78723cf1c0cf563fb3084c20647f7ee690
SUNGWHANYOON <ysh0214727@naver.com>
2022-09-23 22:46:50 +0900

    main logo fix

--- src/Logo.js
+++ src/Logo.js
@@ -9,8 +9,8 @@ export default function Logo() {
      }
     return ( 
         <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{delay:1}} >
-            <div className='Logo'>
-                    <img src='img/hospital.png' alt='Cannot_Found' onClick={onclicked}>
+            <div class='Logo'>
+                    <img src='img/main_logo.png' alt='Cannot_Found' onClick={onclicked}>
                     </img>
             </div>
         </motion.div>
