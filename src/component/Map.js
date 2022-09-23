9ba3a15e391a3f532b0cd5b5f4881e251db2b7f3
SUNGWHANYOON <ysh0214727@naver.com>
2022-09-23 15:16:42 +0900

    Google map api InfoWindows

--- src/component/Map.js
+++ src/component/Map.js
@@ -3,8 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import database from '../db/data.json'
 import mapper from './mapper.css'
 
-const MyMarker = ({ onClick , $hover}) => {
-
+const MyMarker = ({ onClick, $hover, place }) => {
 
     const [icon, setIcon] = useState('img/Map-icon.png');
     const [iconWidth, setIconWidth] = useState("30px");
@@ -13,13 +12,47 @@ const MyMarker = ({ onClick , $hover}) => {
         setIconWidth("30px");
     }
     return (
-        <img className={$hover ? "icon_hover_on":"icon_hover_off" }
-            style={{ /*height: iconWidth, height: iconWidth,*/ position: 'absolute', transform: 'translate(-50%,-50%)' }}
-            onClick={changeIcon}
-            src={icon}
-        />
+        <div>
+            <img className={$hover ? "icon_hover_on" : "icon_hover_off"}
+                style={{ position: 'absolute', transform: 'translate(-50%,-50%)' }}
+                onClick={changeIcon}
+                src={icon}
+            />
+            {< InfoWindow className="Wrapper"
+                style={{ position: 'absolute', transform: 'translate(-50%,-90%)' }}
+                place={place} />}
+
+        </div>
+            );
+};
+
+
+// InfoWindow component
+const InfoWindow = (props) => {
+    const { place } = props;
+    const infoWindowStyle = {
+        position : "relative",
+        bottom: 170,
+        left: "-110px",
+        width: 220,
+        backgroundColor: "white",
+        boxShadow: "0 2px 7px 1px rgba(0, 0, 0, 0.3)",
+        padding: 10,
+        fontSize: 14,
+        zIndex: 100
+
+    };
+
+    return (
+        <div style={infoWindowStyle}>
+            <div style={{ fontSize: 12 }}>{place.name}</div>
+            <div style={{ fontSize: 12 }}>{"------------------------------------------------------------------"}</div>
+            <div style={{ fontSize: 10 }}>{place.content }</div>
+        </div>
     );
 };
+
+
 export default function SimpleMap() {
 
 
@@ -39,7 +72,7 @@ export default function SimpleMap() {
     return (
         <div style={{ height: '100vh', width: '100%' }}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: "///" }}
+                bootstrapURLKeys={{ key: "" }}
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
                 center={{ lat: 35.79835602, lng: 128.5502627 }}
@@ -48,7 +81,7 @@ export default function SimpleMap() {
             >
                     {database.hospitals.map((data, key) => {
                         return (
-                            <MyMarker key={key} lat={data.lat} lng={data.lng} tooltip='hello' />
+                            <MyMarker key={key} lat={data.lat} lng={data.lng} place={data}  tooltip='hello' />
                                 );
                     } )}
             </GoogleMapReact>
