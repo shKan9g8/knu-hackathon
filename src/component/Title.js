c8756f9d1df428b9224d5089a402594a7ef1420a
SungHyun-Kang <80497157+SungHyun-Kang@users.noreply.github.com>
2022-09-23 15:44:48 +0900

    Update Title.js

--- src/component/Title.js
+++ src/component/Title.js
@@ -1,34 +1,98 @@
 
 import React from 'react';
-import styled from 'styled-components';
-import { useState } from 'react';
-import T1 from './T1.js';
-import T2 from './T2.js';
-import T3 from './T3.js';
-import T4 from './T4.js';
-import T5 from './T5.js';
+import database from '../db/data.json';
 function Title({test}){
   console.log(test);
   console.log(typeof test);
-  if (test == 'first')
+  if (test == '1')
   {
-    return (<T1 />);
+    return (<div>
+      {database.hospitals[0].name}
+    </div>);
   }
-  else if (test == 'second')
+  else if (test == '2')
   {
-    return (<T2 />);
+    return (<div>
+      {database.hospitals[1].name}
+    </div>);
   }
-  else if (test == 'third')
+  else if (test == '3')
   {
-    return (<T3 />);
+    return (<div>
+      {database.hospitals[2].name}
+    </div>);
   }
-  else if (test == 'fourth')
+  else if (test == '4')
   {
-    return (<T4 />);
+    return (<div>
+      {database.hospitals[3].name}
+    </div>);
   }
-  else if (test == 'fifth')
+  else if (test == '5')
   {
-    return (<T5 />);
+    return (<div>
+      {database.hospitals[4].name}
+    </div>);
+  }
+  else if (test == '6')
+  {
+    return (<div>
+      {database.hospitals[5].name}
+    </div>);
+  }
+  else if (test == '7')
+  {
+    return (<div>
+      {database.hospitals[6].name}
+    </div>);
+  }
+  else if (test == '8')
+  {
+    return (<div>
+      {database.hospitals[7].name}
+    </div>);
+  }
+  else if (test == '9')
+  {
+    return (<div>
+      {database.hospitals[8].name}
+    </div>);
+  }
+  else if (test == '10')
+  {
+    return (<div>
+      {database.hospitals[9].name}
+    </div>);
+  }
+  else if (test == '11')
+  {
+    return (<div>
+      {database.hospitals[10].name}
+    </div>);
+  }
+  else if (test == '12')
+  {
+    return (<div>
+      {database.hospitals[11].name}
+    </div>);
+  }
+  else if (test == '13')
+  {
+    return (<div>
+      {database.hospitals[12].name}
+    </div>);
+  }
+  else if (test == '14')
+  {
+    return (<div>
+      {database.hospitals[13].name}
+    </div>);
+  }
+  else if (test == '15')
+  {
+    return (<div>
+      {database.hospitals[14].name}
+    </div>);
   }
 };
 
