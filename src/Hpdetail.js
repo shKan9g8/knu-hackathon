168a62127352b7f98f61d936b12ba214df28dd6e
solorsystem1 <39077221+solorsystem1@users.noreply.github.com>
2022-09-25 01:44:30 +0900

    update Hpdetail

--- src/Hpdetail.js
+++ src/Hpdetail.js
@@ -1,6 +1,7 @@
 import React,{useRef, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
 import "./Hpdetail.css";
 import Information from './component/Information';
 import data from './db/data.json'
@@ -8,23 +9,25 @@ import LogoMenu from './component/LogoMenu';
 
 export default function Hpdetail() {
     document.body.style.padding=0;
+
     const {id}=useParams();
     return ( 
         <div>
             <LogoMenu></LogoMenu>
+            <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{delay:1}} >
                 <div className='container'>
-                    <div>
+                    <div className='hospital-img'>
                         {id>=1&id<=15?<img src={"img/h"+id+".png"} alt='Cannot_Found' ></img>:<>잘못된 출력</>}
+                        <ul>{data.hospitals[id-1].name}</ul>
                     </div>
                     <div>
-                        <Link to='/main'><button style={{"backgroundColor":"gray"}}>뒤로가기</button></Link>
                         { data.hospitals.map((hospital,index)=>{
                             if (hospital.id==id){
                                 return(
-                                    <li key={index} style={{ whiteSpace: 'pre-wrap'}}>
+                                    <ul key={index} style={{ whiteSpace: 'pre-wrap'}}>
                                     {hospital.content}
                                     {/*hospital.detail.map((cont)=>cont)*/}
-                                    </li>
+                                    </ul>
                                 );
                                 }
                             }
@@ -36,6 +39,7 @@ export default function Hpdetail() {
                     </div>
 
                 </div>
+            </motion.div>
                 </div>
     );
 } 
