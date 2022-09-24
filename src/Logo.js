f7629721078e9d56202af3970792502c928df21f
solorsystem1 <39077221+solorsystem1@users.noreply.github.com>
2022-09-24 16:15:01 +0900

    add Logo animation

--- src/Logo.js
+++ src/Logo.js
@@ -3,16 +3,25 @@ import "./Logo.css";
 import { motion } from "framer-motion";
 
 export default function Logo() {
-
+    document.body.style.padding=0;
+    document.body.style.backgroundColor="white";
      const onclicked=()=>{
         window.location.assign('/Main')
      }
     return ( 
-        <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{delay:1}} >
-            <div class='Logo'>
-                    <img src='img/main_logo.png' alt='Cannot_Found' onClick={onclicked}>
+        <div className='divide' onClick={onclicked}>
+            <div class='Others-img'>
+                    <img src='img/h9.png' alt='Cannot_Found' style= {{"animationDelay": "1s"}}>
+                    </img>
+                    <img src='img/h10.png' alt='Cannot_Found' style= {{"animationDelay": "2s"}}>
+                    </img>
+                    <img src='img/h12.png' alt='Cannot_Found' style= {{"animationDelay": "3s"}}>
                     </img>
             </div>
-        </motion.div>
+            <div class='Logo-img'>
+                    <img src='img/main_logo.png' alt='Cannot_Found'>
+                    </img>
+            </div>
+        </div>
     );
 }
