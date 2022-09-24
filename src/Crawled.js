667572f0b42a59937d994d47560ce6d1d09d8d69
SUNGWHANYOON <ysh0214727@naver.com>
2022-09-24 18:17:05 +0900

    Add Crawled page icon

--- src/Crawled.js
+++ src/Crawled.js
@@ -35,6 +35,13 @@ export default function Crawled() {
                                     <a href={data.link}>바로가기</a><br/>{data.date}</div>
                                     */
     return ( 
+        <div>
+            <div class="label" style={{ "height": "20px", "width": "99vw", "background": "#a5090b" }}></div>
+            <div class="blank" style={{ "height": "70px", "width": "99vw", "background": "white" }}>
+                <p align="left"><a href="http://localhost:3000/Main"><img src='img/Deagu.jpg' style={{ "height": "60px", "width": "260px", "float": "left", "paddingInlineStart": "50px", "paddingInlineEnd": "50px" }} ></img></a></p>
+                <p align="left"><a href="http://localhost:3000/Main"><img src='img/Main_logo.png' style={{ "height": "60px", "width": "60px", "float": "left" }} ></img></a></p>
+                <p aligh="right" style={{ height: "59px", "float": "right", "fontSize": "50px", "paddingInline": "50px" }}><a href="http://localhost:3000/Main">대구시민을 위한 공공의료 서비스 </a></p>
+            </div>
                 <div className='container1'>
                     <div >
                         {crawled_id!=0?<img className='img-item' src={`./img/${crawled_id}.png`} alt='Cannot_Found' ></img>:<></>}
@@ -71,6 +78,9 @@ export default function Crawled() {
                     <Link to="/main">뒤로가기</Link>
                     </div>
                     
-                </div>
+            </div>
+            <div class="blank" style={{ "height": "60px", "width": "99vw", "background": "white" }}></div>
+            <div class="label" style={{ "height": "20px", "width": "99vw", "background": "#a5090b" }}></div>
+        </div>
     );
 } 
